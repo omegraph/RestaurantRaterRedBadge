e9949725898c92f7d4bec5f5d5715d938e40d802
Samir Omerani <omegraph@gmail.com>
2021-10-06 20:34:58 -0400

    Add Functionality

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -10,11 +10,32 @@ namespace RestaurantRater.Controllers
     public class RestaurantController : Controller
     {
         private RestaurantDbContext _db = new RestaurantDbContext();
+
         // GET: Restaurant/index
         public ActionResult Index()
         {
             return View(_db.Restaurants.ToList());
         }
 
+        // GET: Restaurant/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Restaurant/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Restaurant restaurant)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Restaurants.Add(restaurant);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(restaurant);
+        }
     }
 }
